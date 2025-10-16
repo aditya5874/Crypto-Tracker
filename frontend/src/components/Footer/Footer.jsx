4b49d31da96beae81ebd746f3c7cf2f0f9024e78
aditya5874 <adityagupta5874@gmail.com>
2025-10-17 00:22:34 +0530

    Add creator name to footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,6 +4,8 @@ const Footer = () => {
   return (
     <div className="footer">
       <p>Copyright @ 2024,CryptoTracker -All Rights Reserved</p>
+      <p>Made by -Aditya Gupta</p>
+      
     </div>
   );
 };
